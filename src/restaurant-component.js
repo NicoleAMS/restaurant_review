8c039b0e340c759319f670cfea05d2f4abef4108
NicoleAMS <nicole.sentis@gmail.com>
2020-01-22 16:30:29 +0000

    basic styles for homepage

--- src/restaurant-component.js
+++ src/restaurant-component.js
@@ -1,13 +1,16 @@
 class RestaurantComponent extends HTMLElement {
-	
-	set restaurant(restaurant) {
-		console.log(restaurant);
-		this.innerHTML = `
-			<h4>${restaurant.restaurantName}</h4>
-			<p>${restaurant.address}</p>
-			<p>${restaurant.average} stars</p>
+  set restaurant(restaurant) {
+    console.log(restaurant);
+    this.innerHTML = `
+		<div class="card bg-default my-2">
+			<h4 class="card-header text-primary bg-light">${restaurant.restaurantName}</h4>
+			<div class="card-body">
+				<p>${restaurant.address}</p>
+				<p class="text-primary">${restaurant.average} stars</p>
+			</div>
+		</div>
 		`;
-	}
+  }
 }
 
 customElements.define("restaurant-component", RestaurantComponent);
