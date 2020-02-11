790e312d81c63f3183f0ff288e4ee233a5458f04
NicoleAMS <nicole.sentis@gmail.com>
2020-02-11 18:13:48 +0000

    splits onBoundsChanged function into 3 functions

--- src/google_maps.js
+++ src/google_maps.js
@@ -29,40 +29,47 @@ export function initMap() {
 
   // set lat/lng of NE & SW corners every time the map becomes idle after dragging, panning or zooming
   google.maps.event.addListener(map, "idle", function() {
-    onBoundsChanged();
+    let viewportBounds = setViewportBounds(map);
+    let restaurantsOnMap = setRestaurantsOnMap(viewportBounds);
+    displayRestaurants(restaurantsOnMap, map);
   });
 
-  function onBoundsChanged() {
-    console.log('bounds changed');
-    googleMap = map;
-    var bounds = map.getBounds();
-    var NE = bounds.getNorthEast();
-    var SW = bounds.getSouthWest();
-    var SWLatLng = new google.maps.LatLng(SW.lat(), SW.lng());
-    var NELatLng = new google.maps.LatLng(NE.lat(), NE.lng());
-
-    // set corner coordinates of the map
-    var viewportBounds = new google.maps.LatLngBounds(SWLatLng, NELatLng);
-
-    // check if restaurant is currently visible on the map
-    restaurantsOnMap = [];
-    allRestaurants.forEach(restaurant => {
-      var restaurantLatLng = new google.maps.LatLng({lat: restaurant.lat, lng: restaurant.long});
-      if (viewportBounds.contains(restaurantLatLng)) {
-        restaurantsOnMap.push(restaurant);
-      } 
-    });
-
-    // destroy existing restaurant-components
-    const restaurantColumn = document.getElementById("restaurantCol");
-    restaurantColumn.innerHTML = '';
-
-    restaurantsOnMap.forEach(restaurant => {
-      // display restaurants visible on the map
-      displayRestaurant(restaurant, restaurantColumn);
-      // adds markers on the  map for each visible restaurant
-      addRestaurantMarker(restaurant, map);
-    });
-  }
 }
 
+function setViewportBounds(map) {
+  googleMap = map;
+  var bounds = map.getBounds();
+  var NE = bounds.getNorthEast();
+  var SW = bounds.getSouthWest();
+  var SWLatLng = new google.maps.LatLng(SW.lat(), SW.lng());
+  var NELatLng = new google.maps.LatLng(NE.lat(), NE.lng());
+
+  // set corner coordinates of the map
+  var viewportBounds = new google.maps.LatLngBounds(SWLatLng, NELatLng);
+  return viewportBounds;
+}
+
+function setRestaurantsOnMap(viewportBounds) {
+  // check if restaurant is currently visible on the map
+  restaurantsOnMap = [];
+  allRestaurants.forEach(restaurant => {
+    var restaurantLatLng = new google.maps.LatLng({lat: restaurant.lat, lng: restaurant.long});
+    if (viewportBounds.contains(restaurantLatLng)) {
+      restaurantsOnMap.push(restaurant);
+    } 
+  });
+  return restaurantsOnMap;
+}
+
+function displayRestaurants(restaurantsOnMap, map) {
+  // destroy existing restaurant-components
+  const restaurantColumn = document.getElementById("restaurantCol");
+  restaurantColumn.innerHTML = '';
+
+  restaurantsOnMap.forEach(restaurant => {
+    // display restaurants visible on the map
+    displayRestaurant(restaurant, restaurantColumn);
+    // adds markers on the  map for each visible restaurant
+    addRestaurantMarker(restaurant, map);
+  });
+}
