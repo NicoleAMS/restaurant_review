8b1cdd11ff0d067e5a906dc54ddbde1131a0081d
NicoleAMS <nicole.sentis@gmail.com>
2020-08-27 18:26:16 +0200

    moves Dotenv plugin to webpack dev config file

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,11 +1,17 @@
 const path = require("path");
 const config = require("./webpack.config");
 const merge = require("webpack-merge");
+const Dotenv = require('dotenv-webpack');
 
 module.exports = merge(config, {
   mode: "development",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist")
-  }
+  }, 
+  plugins: [
+    new Dotenv({
+      path: './.env'
+    })
+  ]
 });
